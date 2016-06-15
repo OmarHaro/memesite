cfd8e1e63d3d6e8fecce25ba1c181e1a7ff25663
OmarHaro <mumu100198@gmail.com>
2016-06-15 17:42:55 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function changer()
+{	
+var keefquotes = ["Man I smoke so much dope I swear I think I'm Smokey", "Industry full of lames I could swear I'm Kobe", "I'm cooler than a cooler", "Say Chief Sosa broke that's a rumor", "If you Ain't Talkin' Money I Don't Wanna Talk"];
+	document.getElementById("demo").innerHTML = keefquotes[parseInt(Math.random()*(keefquotes.length))];
+
+}
